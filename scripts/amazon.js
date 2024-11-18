8e2ef01764348dca0d89d9ac9f7e033fde997617
NewMann <osquare98@gmail.com>
2024-11-18 12:07:49 +0100

    Use product.js files

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,55 +1,27 @@
-const product = [
-    {
-        Image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090
-    }, {
-        Image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        priceCents: 2095
-    }, {
-        Image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        priceCents: 799
-    }
-];
-
 let productsHTML = '';
 
-product.forEach((product) => {
+products.forEach((products) => {
     productsHTML += `
         <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${product.Image}">
+              src="${products.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            ${[product.name]}
+            ${[products.name]}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
+              src="images/ratings/rating-${products.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.count}
+              ${products.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            ${(product.priceCents / 100).toFixed(2)}
+            ${(products.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -80,8 +52,6 @@ product.forEach((product) => {
         </div>
     `;
 });
-
-console.log(productsHTML)
 
 document.querySelector('.js-product-grid')
     .innerHTML = productsHTML
