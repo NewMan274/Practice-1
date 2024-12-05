07915e16d4c5b832eea9665d28bd7584aac780e1
NewMann <osquare98@gmail.com>
2024-12-05 10:38:40 +0100

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,23 +3,8 @@ import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import isSatSun from './exerciese/15-exercise.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummary();
 renderCheckoutHeader();
-
-let date = dayjs();
-console.log(date.format('dddd, MMMM D'));
-console.log(isSatSun(date));
-
-date = dayjs().add(2, 'day');
-console.log(date.format('dddd, MMMM D'));
-console.log(isSatSun(date));
-
-date = dayjs().add(4, 'day');
-console.log(date.format('dddd, MMMM D'));
-console.log(isSatSun(date));
-
-date = dayjs().add(6, 'day');
-console.log(date.format('dddd, MMMM D'));
-console.log(isSatSun(date));
