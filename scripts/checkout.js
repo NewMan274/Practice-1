3cdf73acbd2064eb327426e205418b7be878b3e2
NewMann <osquare98@gmail.com>
2024-12-06 10:34:33 +0100

    Convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,7 @@ import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import isSatSun from './exerciese/15-exercise.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
